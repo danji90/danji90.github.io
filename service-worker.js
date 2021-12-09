23ffb8478db27f96a58bcf16877ba7dc21558d18
danji90 <danji90@users.noreply.github.com>
2021-12-09 23:03:34 +0000

    deploy: 26dbc7d04bbfa872c3afbf55047f8f118ddf26e2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c36566e24363d5d77b65bc7c305061c6.js"
+  "/precache-manifest.7ebff4e0f5fcd7fa2cd29a5898375442.js"
 );
 
 self.addEventListener('message', (event) => {
